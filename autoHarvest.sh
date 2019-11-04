d7be1e6171ed17c661283b382a2f8dea67e4c684
liuwilliam <liuwilliam.zh@gmail.com>
2019-11-04 16:35:35 +0800

    perf(autoHarvest.sh): Tweak some process order, to make if more efficient.

--- autoHarvest.sh
+++ autoHarvest.sh
@@ -26,27 +26,29 @@ getEggsRequest=$(curl --cookie ${projectPath}/cookies.txt ${getEggs} | iconv -f 
 echo -e "---- "$(date)" ---- \n"$getEggsRequest >> ${projectPath}/log.txt
 
 response=$(curl --cookie ${projectPath}/cookies.txt "http://jlpzj.net/plugin.php?id=jneggv2" | iconv -f gbk -t utf-8)
-
+hashStr=$(echo ${response} | grep -o "getegg&formhash=[a-zA-Z0-9]\{8\}" | grep -o "formhash=[a-zA-Z0-9]\{8\}")
 eggs=$(echo ${response} | grep -o "鸡蛋 : [0-9]*粒" | grep -o "[0-9]*")
-
-money=$(echo ${response} | grep -o "家元: [0-9]* 元" | grep -o "[0-9]*")
-
 postBaseUrl="http://jlpzj.net/plugin.php?id=jneggv2:jneggv2&inajax=1"
 
 sellEggs="${hashStr}&changeeggsubmit=yes&handlekey=changeeggsubmit&referer=http%3A%2F%2Fjlpzj.net%2Fplugin.php%3Fid%3Djneggv2&paymount=60"
-
-buyHen="${hashStr}&buychicsubmit=yes&handlekey=editmine&referer=http%3A%2F%2Fjlpzj.net%2Fplugin.php%3Fid%3Djneggv2&paymount=1"
-
-echo "---- "$(date)" ----" >> ${projectPath}/log.txt
-echo "Eggs: ${eggs} Money: ${money}" >> ${projectPath}/log.txt
 
 if [[ ${eggs} -gt 60 ]]; then
     postBody=${sellEggs}
     postRequest
 fi
 
+response=$(curl --cookie ${projectPath}/cookies.txt "http://jlpzj.net/plugin.php?id=jneggv2" | iconv -f gbk -t utf-8)
+hashStr=$(echo ${response} | grep -o "getegg&formhash=[a-zA-Z0-9]\{8\}" | grep -o "formhash=[a-zA-Z0-9]\{8\}")
+money=$(echo ${response} | grep -o "家元: [0-9]* 元" | grep -o "[0-9]*")
+
+buyHen="${hashStr}&buychicsubmit=yes&handlekey=editmine&referer=http%3A%2F%2Fjlpzj.net%2Fplugin.php%3Fid%3Djneggv2&paymount=1"
+
 if [[ ${money} -gt 20 ]]; then
     postBody=${buyHen}
     postRequest
 fi
+
+echo "---- "$(date)" ----" >> ${projectPath}/log.txt
+echo "Eggs: ${eggs} Money: ${money}" >> ${projectPath}/log.txt
+
 
