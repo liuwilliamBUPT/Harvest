3461ae84f7716c3d84302e69cd21c28ac77c09b4
root <root@DESKTOP-3RRBCVQ.localdomain>
2019-11-04 09:29:37 +0800

    perf(autoHarvest.sh): Sell eggs after collect eggs.

--- autoHarvest.sh
+++ autoHarvest.sh
@@ -20,11 +20,16 @@ response=$(curl --cookie ${projectPath}/cookies.txt "http://jlpzj.net/plugin.php
 
 hashStr=$(echo ${response} | grep -o "getegg&formhash=[a-zA-Z0-9]\{8\}" | grep -o "formhash=[a-zA-Z0-9]\{8\}")
 
+getEggs="http://jlpzj.net/plugin.php?id=jneggv2:jneggv2&do=getegg&${hashStr}&infloat=yes&handlekey=joincom&inajax=1&ajaxtarget=fwin_content_joincom"
+
+getEggsRequest=$(curl --cookie ${projectPath}/cookies.txt ${getEggs} | iconv -f gbk -t utf-8)
+echo -e "---- "$(date)" ---- \n"$getEggsRequest >> ${projectPath}/log.txt
+
+response=$(curl --cookie ${projectPath}/cookies.txt "http://jlpzj.net/plugin.php?id=jneggv2" | iconv -f gbk -t utf-8)
+
 eggs=$(echo ${response} | grep -o "鸡蛋 : [0-9]*粒" | grep -o "[0-9]*")
 
 money=$(echo ${response} | grep -o "家元: [0-9]* 元" | grep -o "[0-9]*")
-
-getEggs="http://jlpzj.net/plugin.php?id=jneggv2:jneggv2&do=getegg&${hashStr}&infloat=yes&handlekey=joincom&inajax=1&ajaxtarget=fwin_content_joincom"
 
 postBaseUrl="http://jlpzj.net/plugin.php?id=jneggv2:jneggv2&inajax=1"
 
@@ -32,11 +37,8 @@ sellEggs="${hashStr}&changeeggsubmit=yes&handlekey=changeeggsubmit&referer=http%
 
 buyHen="${hashStr}&buychicsubmit=yes&handlekey=editmine&referer=http%3A%2F%2Fjlpzj.net%2Fplugin.php%3Fid%3Djneggv2&paymount=1"
 
-getEggsRequest=$(curl --cookie ${projectPath}/cookies.txt ${getEggs} | iconv -f gbk -t utf-8)
-echo -e "---- "$(date)" ---- \n"$getEggsRequest >> ${projectPath}/log.txt
-
-echo -e "---- "$(date)" ----" >> ${projectPath}/log.txt
-echo -e "Eggs: ${eggs} Money: ${money}" >> ${projectPath}/log.txt
+echo "---- "$(date)" ----" >> ${projectPath}/log.txt
+echo "Eggs: ${eggs} Money: ${money}" >> ${projectPath}/log.txt
 
 if [[ ${eggs} -gt 60 ]]; then
     postBody=${sellEggs}
